21ce026b0e9c474833ebe53bdbe2e85cd8dc27fb
donaldk12345 <fotsoyvesdonald@gmail.com>
2022-04-02 12:58:36 +0200

    premier commit

---
+++ src/test/java/location/groupe4/vehicule/VehiculeApplicationTests.java
@@ -0,0 +1,13 @@
+package location.groupe4.vehicule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiculeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
